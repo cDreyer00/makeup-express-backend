54507109d1ea7d332017f93292aa760c731873e0
cdreyer <cristianbr.dreyer@gmail.com>
2023-12-15 00:09:47 -0300

    analysis generation update and creating images

--- index.js
+++ index.js
@@ -1,131 +1,123 @@
+/// OPENAI DOC
+/*
+Response
+{
+  "created": 1589478378,
+  "data": [
+    {
+      "url": "https://..."
+    },
+    {
+      "url": "https://..."
+    }
+  ]
+}
+*/
+/// DOC END
+
 require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
-const app = express();
 const OpenAi = require('openai');
+const fs = require('fs');
 
 const sd_key = process.env.STABLE_DIFFUSION_KEY;
+
 const openai_key = process.env.OPENAI_KEY;
-const key = process.env.API_KEY;
 const openai = new OpenAi({ apiKey: openai_key });
+const completions = openai.chat.completions;
 
-// app.use(express.static(path.join(__dirname, 'public')));
+const upload = multer({ storage: multer.memoryStorage() });
 
-// const upload = multer({ storage: multer.memoryStorage() });
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+const models = {
+  gpt4: "gpt-4",
+  gpt4Vision: "gpt-4-vision-preview",
+  dallE: "dall-e-3",
+}
 
-// app.post('/upload', upload.single('img'), (req, res) => {
-//   console.log(req.file);
-//   console.log(req.file.buffer);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.post('/upload', upload.single('img'), async (req, res) => {
+  const img = req.file.buffer.toString('base64');
+  const prompt = req.body.prompt;
+  try {
+    const analyses = await GetAnalyse(img);
+    const reco = await GetRecomendations(analyses);
+    const img_url = await GenerateImages(reco);
+    return res.json({ analyses, reco, img_url });
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send('Something went wrong');
+  }
+});
 
-// app.listen(3000, () => {
-//   console.log('Server is running on http://localhost:3000');
-// });
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
 
+const instructions = `
+Você é Make.Express: um Conselheiro de estilo e moda personalizado que oferece links de compra. Make.Express é um assistente de estilo inovador, projetado para fornecer conselhos personalizados de maquiagem e moda. Quando um usuário carrega sua foto, o Make.Express analisa suas características faciais e vestuário para gerar sugestões de maquiagem e roupas sob medida. Após gerar as recomendações e imagens usando a ferramenta dalle, o Make.Express usa a ferramenta de navegador para procurar links de produtos relacionados às sugestões de estilo na Amazon brasileira, fornecendo links de compra para os usuários ao final de cada resposta. Esta representação visual e os links de compras ajudam os usuários a visualizarem e adquirirem os estilos recomendados. O serviço prioriza a privacidade do usuário e a segurança dos dados, garantindo uma jornada de estilo segura e personalizada.
+`;
 
-async function main() {
-
-  const completions = openai.chat.completions;
-
-  // use gtp4 vision to create an analysis of the person in the image
-  const analysisRes = await completions.create({
-    model: "gpt-4-vision-preview",
+async function GetAnalyse(img, max_tokens = 400) {
+  const completion = await openai.chat.completions.create({
+    model: models.gpt4Vision,
     messages: [
       {
-        role: "assistant",
-        content: [
-          { type: "text", text: "Make.Express, previously known as Beauty Muse, has evolved to provide even more personalized beauty advice. Now, users can upload photos of themselves to receive customized suggestions for makeup and hair that not only suit their facial features but also complement their attire. When users share a photo, Make.Express will analyze their outfit and overall appearance to offer beauty solutions that harmonize with their personal style and the occasion they're dressing for. This feature is designed to enhance the user experience, providing a tailored approach to beauty advice. The tool prioritizes user privacy and data security, ensuring that all recommendations are made with the utmost respect for personal information. Make.Express combines visual analysis with fashion and beauty expertise to deliver unique and cohesive styling suggestions." },
-        ],
+        role: "system",
+        content: instructions
       },
       {
         role: "user",
         content: [
-          { type: "image_url", image_url: "https://i.postimg.cc/zB7Wndjk/eu.png" },
-          { type: "text", text: "create an descriptive analysis based on the image" },
-        ],
-      }],
-    max_tokens: 400,
-  });
-
-  var analysis = analysisRes.choices[0].message.content;
-  console.log(`analysis: ${analysis}`);
-
-  const extractionRes = await completions.create({
-    model: "gpt-4",
-    messages: [
-      {
-        role: "user",
-        content: [
-          { type: "text", text: analysis },
-          { type: "text", text: "extract a list of some key visual articles" }
+          { type: "text", text: "I will send an image of a person and I want that you create a text describing the overall person's visual aspects and general apperance." },
+          { type: "image", image: img }
         ]
       }
     ],
-    max_tokens: 400,
-  });
+    max_tokens,
+  })
 
-  var extraction = extractionRes.choices[0].message.content;
-  console.log(`extraction: ${extraction}`);
-
-  var generateLinksRes = await completions.create({
-    model: "gpt-4",
-    messages: [
-      {
-        role: "user",
-        content: [
-          { type: "text", text: extraction },
-          { type: "text", text: "send a list of makeup items" }
-        ]
-      }
-    ],
-    max_tokens: 400,
-  });
-
-  var generateLinks = generateLinksRes.choices[0].message.content;
-  console.log(`generateLinks: ${generateLinks}`);
-
-  var promptRest = await completions.create({
-    model: "gpt-4",
-    messages: [
-      {
-        role: "user",
-        content: [
-          { type: "text", text: generateLinks },
-          { type: "text", text: "create a text prompt in order to be used on some AI image generation" }
-        ]
-      }
-    ],
-    max_tokens: 400,
-  });
-
-  var prompt = promptRest.choices[0].message.content;
-  console.log(`prompt: ${prompt}`);
-
-  // const gptResponse = await completions.create({
-  //   model: "gpt-4",
-  //   messages: [
-
-  //     {
-  //       role: "user",
-  //       content: [
-  //         // { type: "text", text: "Create a shot description about the person in the sent image containing visual aspects and gender and in the description add the visual tags of the object. Send the text response like prompt format" },
-  //         // { type: "text", text: "add to description: Red Lipstick" },
-  //         // { type: "image_url", image_url: "https://i.postimg.cc/zB7Wndjk/eu.png" },
-  //       ],
-  //     }],
-  //   max_tokens: 200,
-  // });
-  // console.log(gptResponse.choices[0]);
+  return completion.choices[0].message.content;
 }
 
-main().catch((error) => {
-  console.error(error);
-});
+async function GetRecomendations(prompt, max_tokens = 400) {
+  const completion = await openai.chat.completions.create({
+    model: models.gpt4,
+    messages: [
+      {
+        role: "user",
+        content: prompt
+      },
+      {
+        role: "user",
+        content: "send me a list of visual elements like makeups and/or hairstyles that would suit the person in the description"
+      }
+    ],
+    max_tokens,
+  })
+
+  return completion.choices[0].message.content;
+}
+
+// generate a image identical to the person in the picture with the chagnes listed on recomendations response  
+async function GenerateImages(prompt) {
+  const response = await openai.images.generate({
+    model: models.dallE,
+    prompt: prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+  image_url = response.data[0].url;
+  return image_url;
+}
+
+
